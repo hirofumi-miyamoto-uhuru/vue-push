5f3fd6dc4691240f1c88b41a59daa7cff16de9a9
hirofumi-miyamoto-uhuru <miyamoto.hirofumi@up.uhuru.jp>
2023-10-04 16:00:15 +0900

    feat: set pwa condition

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,15 @@ import { registerSW } from 'virtual:pwa-register';
 registerSW();
 
 createApp(App).mount('#app')
+
+const isPWA = window.matchMedia("(display-mode: standalone)").matches;
+
+isPWA
+  ? addElement("div", "これは PWA です")
+  : addElement("div", "これは PWA ではありません");
+
+function addElement(el: string, inner: string) {
+  const createdElement = document.createElement(el);
+  createdElement.innerHTML = inner;
+  document.body.append(createdElement);
+}
